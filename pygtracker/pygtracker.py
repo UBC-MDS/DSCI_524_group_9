949e31f65e0cf8d4ea622a42400e04287ac2a76a
Tran Doan Khanh Vu <tdkhanhvu@gmail.com>
2021-02-25 18:25:07 -0800

    Create skeleton for python file

---
+++ pygtracker/pygtracker.py
@@ -0,0 +1,31 @@
+# Author: Javairia, Jianru, Yanhua and Vu
+
+class GradeTracker():
+    """[To be completed: Docstring for the class]
+    """
+    courses = None
+    grades = None
+
+    def __init__(self):
+        """see help(GradeTracker)"""
+        pass
+
+    # Start: Register the courses
+
+    # End: Register the courses
+
+    # Start: Record the grade for students
+
+    # End: Record the grade for students
+
+    # Start: Summarize the grades by courses
+
+    # End: Summarize the grades by courses
+
+    # Start: Summarise the grades by students
+
+    # End: Summarise the grades by students
+
+    # Start: Suggest grade adjustment
+
+    # End: Suggest grade adjustment
