f51b8842ea8b4621cad34ea92fa5a6cbbd999f8a
Javairia Raza <jr.jem19@gmail.com>
2021-02-25 21:24:58 -0800

    Add empty function 3 and its docstring

--- pygtracker/pygtracker.py
+++ pygtracker/pygtracker.py
@@ -1,8 +1,9 @@
 # Author: Javairia, Jianru, Yanhua and Vu
 
-class GradeTracker():
-    """[To be completed: Docstring for the class]
-    """
+
+class GradeTracker:
+    """[To be completed: Docstring for the class]"""
+
     courses = None
     grades = None
 
@@ -23,6 +24,23 @@ class GradeTracker():
     # End: Summarize the grades by courses
 
     # Start: Summarise the grades by students
+    def student_ranker(course_id="all", n=10, descending=True):
+        """[Calculates the average grade for a specified number of students and ranks them in descending order for a specific course or for the entire program completed thus far]
+
+        Args:
+            course_id (int): [the course ID for which the ranking is calculated for by student.
+            The default option will calculate the ranking for the entire program]. Defaults to "all".
+
+            n(int): [the number of students to rank]. Defaults to 10.
+
+            ascending(bool): [a boolean value to decide if the rank should be in ascending or descending order.
+            The default option is decending order]. Defaults to True.
+
+        Returns:
+            [pandas.DataFrame]: [a dataframe containing the rank of a specified number of students in descending or ascending order
+            for a given course or for the whole program]
+        """
+        return student_rank_df
 
     # End: Summarise the grades by students
 
