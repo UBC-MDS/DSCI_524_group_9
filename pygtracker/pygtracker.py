eb01839f860539099931e404a8e9f3a2c258f626
Tran Doan Khanh Vu <tdkhanhvu@gmail.com>
2021-02-26 20:41:57 -0800

    Reformat docstrings to make it consistent

--- pygtracker/pygtracker.py
+++ pygtracker/pygtracker.py
@@ -10,10 +10,10 @@ class GradeTracker:
 
     Attributes
     ----------
-    courses : DataFrame
-      The dataframe containing component weights for each course.
-    grades : DataFrame
-      The dataframe containing grades for each student in each course.
+    courses: DataFrame
+        The dataframe containing component weights for each course.
+    grades: DataFrame
+        The dataframe containing grades for each student in each course.
     """
 
     courses = None
@@ -30,8 +30,11 @@ class GradeTracker:
         Parameters
         ----------
         df: DataFrame
-          A tidy dataframe with course_id, assessments (lab1, quiz1, etc.) and their weights (eg, 0.2).
-          All assessment components for a course must sum up to 1.
+            A tidy dataframe:
+                course_id: str
+                assessment_id: str, ex: lab1, quiz1
+                weight: float, ex: 0.2
+            All assessment components for a course must sum up to 1.
 
         Returns
         -------
@@ -46,7 +49,11 @@ class GradeTracker:
         Parameters
         ----------
         df: DataFrame
-          A tidy dataframe with course_id, student_id, assessment_id and grade.
+            A tidy dataframe:
+                course_id: str
+                student_id: str
+                assessment_id: str
+                grade: float
 
         Returns
         -------
@@ -54,20 +61,24 @@ class GradeTracker:
         """
         return None
 
-    def generate_course_statistics(self, course_id):
+    def generate_course_statistics(self, course_ids):
         """
         Calculate the summary statistics for specified courses including mean, median and quantiles.
 
         Parameters
         ----------
-        course_id: list
+        course_ids: list of str
             A list including all course IDs for which the summary statistics are calculated for.
 
         Returns
         ----------
         DataFrame
-            A dataframe containing the summary statistics for specified courses.
-            Row Index is course id. Column Index will have the mean, 1st-quantile, median, 3rd-quantile for the specified course.
+            A dataframe containing the summary statistics for specified courses:
+                course_id: str
+                mean: float
+                1st-quantile: float
+                median: float
+                3rd-quantile: float
         """
         return None
 
@@ -77,7 +88,7 @@ class GradeTracker:
 
         Parameters
         ----------
-        method: {"mean", "1st-quantile", "median", "3rd-quantile"}
+        method: {"mean", "1st-quantile", "median", "3rd-quantile"}, default "mean"
             The method applied to rank the courses.
         descending: bool, default True
             A boolean value to decide if the rank should be in descending or ascending order.
@@ -85,21 +96,23 @@ class GradeTracker:
         Returns
         -------
         DataFrame
-            A dataframe containing the rank for specified courses.
+            A dataframe containing the rank for specified courses:
+                course_id: str
+                rank: int
+                grade: float
         """
         return None
 
     def rank_students(self, course_id="all", n=10, descending=True):
         """
-        Calculate a student's rank in a course or the program.
-
-        Calculates the average grade for a specified number of students and ranks them in
+        Calculate the average grade for a specified number of students and ranks them in
         ascending/descending order for a specific course or for the entire program completed thus far.
 
         Parameters
         ----------
         course_id: str, default "all"
-            The course id for which the ranking is calculated for. Default will provide the ranking for the entire program completed thus far.
+            The course id for which the ranking is calculated for. Default will provide the ranking for
+            the entire program completed thus far.
         n: int, default 10
             The number of students to rank.
         descending: bool, default True
@@ -108,8 +121,10 @@ class GradeTracker:
         Returns
         -------
         DataFrame
-            A dataframe containing the rank of students by average grade.
-            Row Index is student id, and Column Index is average grade.
+            A dataframe containing the rank of students by average grade:
+                student_id: str
+                rank: int
+                grade: float
         """
         return None
 
@@ -123,18 +138,21 @@ class GradeTracker:
         Parameters
         ----------
         course_id: str
-          The id of the course to be adjusted.
+            The id of the course to be adjusted.
         benchmark_course: float, default 0.9
-          The benchmark of which the average grade for the whole course must meet or exceed.
+            The benchmark of which the average grade for the whole course must meet or exceed.
         benchmark_lab: float, default 0.85
-          The benchmark of which the average grade for each lab must meet or exceed.
+            The benchmark of which the average grade for each lab must meet or exceed.
         benchmark_quiz: float, default 0.85
-          The benchmark of which the average grade for each quiz must meet or exceed.
+            The benchmark of which the average grade for each quiz must meet or exceed.
 
         Returns
         -------
         DataFrame
-          A dataframe containing newly adjusted grades for students in a course.
-          Row Index is student id, and Column Index is the assessment id.
+            A dataframe containing newly adjusted grades for students in a course:
+                student_id: str
+                assessment1: float
+                assessment2: float
+                ...
         """
         return None
